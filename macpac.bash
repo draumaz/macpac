09d1bc16d283b2b70fc9cf410b1032eddb3aea2a
Emma <77967471+draumaz@users.noreply.github.com>
2023-04-10 13:18:23 -0700

    Create macpac.bash

---
+++ macpac.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash -e
+
+case ${2} in "") exit 1 ;; esac
+
+BASENAME=$(
+  echo ${2} | \
+    tr '/' '\n' | \
+    tail -1 | \
+    sed 's/.macpacz//g' | \
+    sed 's/-.*//g'
+)
+
+REPO_PATH="" # point to a directory containing .pkgz files
+
+PKG_PATH=$(find ${REPO_PATH} -name '*.pkgz' | tail -1)
+
+# non functional
+uninstall() {
+  PROTECTED="local:locale:bin:include:lib:doc:opt:share:man"
+  for i in `tar -tf ${PKG_PATH}`; do
+    for j in `echo ${PROTECTED} | tr ':' '\n'`; do
+      case "${i}" in *${j}/) echo $i ;; *) ;; esac
+    done
+  done
+}
+
+install() {
+  printf "installing ${BASENAME}... "
+
+  tar -xpf ${PKG_PATH} \
+    --strip-components=1 \
+    -C /opt
+
+  echo "done."
+}
+
+case "${1}" in
+  i) install   "${@}" ;; 
+  u) uninstall "${@}" ;;
+esac || "${@}"
